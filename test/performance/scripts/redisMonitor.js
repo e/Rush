77e9597885a10ae56bf3019d3fc6e9ee18b5e803
David Barriga Ruano <dbarriga@conwet.com>
2013-06-25 12:02:09 +0200

    Add transactions handled

--- test/performance/scripts/redisMonitor.js
+++ test/performance/scripts/redisMonitor.js
@@ -1,19 +1,22 @@
-var redisModule = require('redis');
+var redis = require('redis');
 var port = process.argv[3];
 var host = process.argv[2];
 
 var redisDb = redis.createClient(port, host);
 
-// Aux functions
 var monitorQueue = function (queue, callback) {
   'use strict';
-  redisDb.llen('wrL:hpri', function (err ,value) {
+  redisDb.llen(queue, function (err ,value) {
     callback(value);
   });
 };
 
+var qBefore = 0;
+
 setInterval(function () {
-  monitorQueue(key, function (value) {
-    console.log('Elements in queue: ' + value);
+  monitorQueue('wrL:hpri', function (value) {
+    console.log('Transactions handled: ' + Math.abs(qBefore - value));
+    console.log('Total elements in queue: ' + value);
+    qBefore = value;
   });
-}, 500)
+}, 1000)
