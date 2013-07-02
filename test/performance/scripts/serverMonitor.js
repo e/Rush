1da6ab7ed81ac20aa03f9a1fe60d95285b522a8b
David Barriga Ruano <dbarriga@conwet.com>
2013-07-02 11:00:01 +0200

    Add script to monitor server performance

---
+++ test/performance/scripts/serverMonitor.js
@@ -0,0 +1,78 @@
+var fs = require('fs');
+
+var pid = process.argv[2];
+
+// Functions to get system usage
+var getUserUsage = function (pid) {
+  'use strict';
+  var data = fs.readFileSync('/proc/' + pid + '/stat');
+
+  var elems = data.toString().split(' ');
+  var utime = parseInt(elems[13]);
+  var stime = parseInt(elems[14]);
+
+  return utime + stime;
+}
+
+var getSysUsage = function () {
+  'use strict';
+  var data = fs.readFileSync('/proc/stat');
+
+  var elems = data.toString().split(' ');
+
+  return parseInt(elems[2]) + parseInt(elems[3]) + parseInt(elems[4]) + parseInt(elems[5]);
+};
+
+var getProcMem = function (pid) {
+  'use strict';
+  var data = fs.readFileSync('/proc/' + pid + '/status');
+
+  var elems = data.toString().split('\n');
+  elems = elems[15].split('\t');
+  elems = elems[1].split(' ');
+
+  return elems[elems.length - 2];
+};
+
+// Functions to monitor a pid
+var monitor = function (pid) {
+  'use strict';
+  var startTimeU,
+      startTimeS,
+      endTimeU,
+      endTimeS,
+      mem;
+
+    startTimeU = getUserUsage(pid);
+    startTimeS = getSysUsage();
+
+  setTimeout(function (startTimeU, startTimeS) {
+    var cpu_sys, cpu_user, percentage = 0, mem = 0;
+
+      endTimeU = getUserUsage(pid);
+      endTimeS = getSysUsage();
+
+      console.log(endTimeS - startTimeS);
+      cpu_sys = endTimeS - startTimeS;
+      cpu_user = endTimeU - startTimeU;
+
+      mem += parseInt(getProcMem(pid));
+      percentage += 100 * (cpu_user / cpu_sys);
+
+    /*var result = {
+      memory:mem,
+      cpu:percentage
+    };
+
+    callback(result);*/
+    console.log('------------------------------------------------------------------------');
+    console.log('CPU: ' + percentage + '%');
+    console.log('Memory:'  + mem + ' bytes');
+    console.log('------------------------------------------------------------------------');
+
+  }.bind({}, startTimeU, startTimeS), 1000);
+};
+
+setInterval(function () {
+  monitor(pid);
+}, 1000);
